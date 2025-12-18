79dc417bf5e88fc5b974a11e5789cac4ae7cffc8
aaryavardhan2002 <aaryavardhan11@gmail.com>
2025-12-18 11:34:42 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+-- Create the 'DataWarehouse' database
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
+
+
+
